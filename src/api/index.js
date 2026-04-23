359d280c1f2d1fcacdb1d02c75791173030afaa5
Adriel Zumaeta <zumaetaadriel2@gmail.com>
2026-04-23 13:31:08 +0000

    fix: error ejecutar en localhost.

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,14 @@
 const http = require("http");
 
-const hostname  = "0.0.0.0";
+const hostname = "0.0.0.0";
 const port = 3000;
 
-const server = http
-.createServer((request,response)=>{
+const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/plain");
     response.end("Hola mundo! Un saludo");
 });
 
 server.listen(port, hostname, () => {
-    console.log('Bienvenido http://${hostname}:${port}/')
+    console.log(`Bienvenido http://${hostname}:${port}/`);
 });
